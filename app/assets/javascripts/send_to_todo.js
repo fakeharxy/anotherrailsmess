b357783ea73fccdca67283034798fc5869289372
harxy <harkerp@gmail.com>
2017-02-25 00:33:15 +0000

    added button to todo list BUT F**CKING DOUBLE CLICK BUG ARRRGRGRGRGRRHGRHGRHRGHRGRHGJRGRJGRJRGJRJGRJRH

--- app/assets/javascripts/send_to_todo.js
+++ app/assets/javascripts/send_to_todo.js
@@ -1,10 +1,8 @@
 var yesterday = new Date((new Date()).valueOf() - 1000 * 60 * 60 * 24);
 
-$(document).on('click', '.todo', function(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
+$(document).on('click', '#todo', function(evt) {
   var paraId = $(this).index('.todo') + 1;
-  $(this).pickadate({
+  $('#todo').pickadate({
     disable: [{
       from: [0, 0, 0],
       to: yesterday

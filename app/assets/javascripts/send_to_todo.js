f61e33a37a860d1047d6c1a3b8dcfaf63074bc87
harxy <harkerp@gmail.com>
2017-02-27 13:16:41 +0000

    improved the editing of the titles

--- app/assets/javascripts/send_to_todo.js
+++ app/assets/javascripts/send_to_todo.js
@@ -3,7 +3,6 @@ var yesterday = new Date((new Date()).valueOf() - 1000 * 60 * 60 * 24);
 $(document).on('click', '.todo', function(evt) {
 
   var paraId = $(this).index('.todo') + 1;
-  console.log(paraId);
   $(this).pickadate({
     disable: [{
       from: [0, 0, 0],

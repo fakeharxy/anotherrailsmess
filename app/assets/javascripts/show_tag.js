88492727a026bd21f46483b6ea74a5da9bc576d0
harxy <harkerp@gmail.com>
2017-04-05 13:26:11 +0100

    added hidden on blur

--- app/assets/javascripts/show_tag.js
+++ app/assets/javascripts/show_tag.js
@@ -1,7 +1,10 @@
 $(document).on('click', '.para', function() {
 
-  $(this).siblings('.tag-field').toggleClass('hidden');
-  
+  $(this).siblings('.tag-field').removeClass('hidden');
+
   $(this).siblings('.tag-field').find('.form-control').focus();
 
+  $(this).siblings('.tag-field').find('.form-control').blur(function() {
+    $(this).closest('.tag-field').addClass('hidden');
+  });
 })
